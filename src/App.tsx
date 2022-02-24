663e2d10be15df38bdb4dc9dfbe33fd57cd1fb34
gabryelfhsoares <gabryelfhsoares@gmail.com>
2022-02-24 16:47:53 -0300

    add first fetch

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,38 @@
-import { useState } from 'react'
+import { Children } from 'react'
+import { useQuery } from 'react-query';
+
+import axios from './services/axios';
+
+type Repository = {
+  full_name: string;
+  description: string;
+}
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  const { data, isFetching} = useQuery('repos', async () => {
+    const response = await axios.get<Repository[]>('/users/GabryelSoares/repos');
+  
+    return response.data;
+  });
 
   return (
-    <h1>Hello world</h1>
+    <ul>
+      {(isFetching) ? (
+        <p>Carregando...</p>
+      ) : (
+        <>          
+          {Children.toArray(
+            data?.map((repo) => (
+              <li>
+                <strong>{repo.full_name}</strong>
+                <p>{repo.description}</p>
+              </li>
+            ))
+          )}
+        </>
+      )}
+    </ul>
   )
 }
 
